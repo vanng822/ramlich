6dd1f76e740fba8a5dc095e2f5108f70a3ff642b
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2024-08-10 15:25:14 +0200

    Remove redudant assertion

--- src/vncalendar/mod.rs
+++ src/vncalendar/mod.rs
@@ -90,18 +90,17 @@ mod tests {
     #[test]
     fn get_year_month_dates_test() {
         let res = get_year_month_dates(2024);
-        /*
+
         for (k, v) in res.iter() {
             println!("{}", *k);
             for d in v {
                 println!("{}", d);
             }
         }
-        */
+
         assert_eq!(12, res.len());
         assert_eq!(31, res.get(&Month::January).unwrap().len());
         assert_eq!(29, res.get(&Month::February).unwrap().len());
-        assert_eq!(31, res.get(&Month::March).unwrap().len());
         assert_eq!(31, res.get(&Month::March).unwrap().len());
         assert_eq!(30, res.get(&Month::April).unwrap().len());
         assert_eq!(31, res.get(&Month::May).unwrap().len());
