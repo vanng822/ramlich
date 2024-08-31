14dacd61f3d899d0b98f61ce1856ac26a85a6c49
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2024-08-31 15:20:53 +0200

    Need to specify the param for swagger ui to work

--- src/requests/mod.rs
+++ src/requests/mod.rs
@@ -2,12 +2,18 @@ use serde::Deserialize;
 use utoipa::IntoParams;
 
 #[derive(IntoParams, Deserialize)]
+#[into_params(parameter_in = Query)]
 pub struct LunarToSolar {
+    // Assume 4 digits year
+    #[param(max_length = 10)]
     pub solar_date: String,
 }
 
 #[derive(Deserialize, IntoParams)]
+#[into_params(parameter_in = Query)]
 pub struct LunarToSolarDates {
+    #[param()]
     pub year: i32,
+    #[param()]
     pub month: Option<u32>,
 }
